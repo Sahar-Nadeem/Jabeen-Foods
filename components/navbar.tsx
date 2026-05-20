1f8c2de48d09679d148972f4bfb13f41a31e2c33
Sahar-Nadeem <112645241+Sahar-Nadeem@users.noreply.github.com>
2026-05-20 17:18:46 +0500

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -25,7 +25,7 @@ export function Navbar() {
           {/* Logo */}
           <Link href="/" className="flex items-center gap-3">
             <div className="flex items-center">
-              <img src="/images/JABEEN FOOD-02.png" alt="Jabeen Foods" className="h-22 w-auto" />
+              <img src="/images/JABEEN FOOD-02.png" alt="Jabeen Foods" className="h-25 w-auto" />
             </div>
           </Link>
 
